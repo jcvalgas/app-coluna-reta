fde5fef562c2ed4505c7980ca1d7cd7a2bd99009
Bruna Bomfim <brunabomfim.bbb@gmail.com>
2022-08-04 20:53:48 -0300

    docs: Swagger installation
    Swagger Implementation
    Class-Validator instalation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle('Coluna Reta')
+    .setDescription('Aplicação para visualização e cadastro de alunos com enfermidade na coluna.')
+    .setVersion('1.0.0')
+    .addTag('status')
+    .addTag('user')
+    .addTag('students')
+    .addTag('instituition')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
