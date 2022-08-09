6d1a344f1a02134da5e26e897a2c3f5181db6039
Astolfo Valentim <97894808+astolfovalentim@users.noreply.github.com>
2022-08-09 20:24:10 -0300

    fix: users branch auth correction

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -26,21 +26,6 @@ export class UsersController {
   }
 
 
-  // @Get(':id')
-  // findOne(@Param('id') id: string) {
-  //   return this.usersService.findOne(id);
-  // }
-
-  // @Patch(':id')
-  // update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-  //   return this.usersService.update(id, updateUserDto);
-  // }
-
-  // @Delete(':id')
-  // remove(@Param('id') id: string) {
-  //   return this.usersService.remove(id);
-  // }
-
   @Get(':id')
   @ApiOperation({
 		summary: "View users by id or email",
